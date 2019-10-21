f641f0c609471ff5864393ea86567e20e9089a64
Andrew Couch <andrew.couch.ia@gmail.com>
2019-10-21 00:55:46 -0500

    Added pics

--- MediumDebateArticle.R
+++ MediumDebateArticle.R
@@ -8,9 +8,56 @@ df <- df %>%
 options(scipen = 999)
 
 
-df %>% unnest_tokens(word, "text", token = "ngrams", n =1)
+df %>% 
+  unnest_tokens(word, "text", token = "ngrams", n =2) %>% 
+  separate(word, c("word1","word2"), sep = " ") %>% 
+  filter(!word1 %in% stop_words$word | !word2 %in% stop_words$word) %>% 
+  unite("bigram", c(word1, word2), sep = " ") %>% 
+  count(character,bigram) %>% 
+  group_by(character) %>% 
+  top_n(n, n = 5) %>% 
+  arrange(character, -n) %>% 
+  ggplot(aes(x = reorder_within(bigram, n, character),#Reorders the words by freq
+             y  = n,
+             fill = character)) + 
+  geom_col() + 
+  scale_x_reordered() + #Reorders the words 
+  facet_wrap(~character, scales ="free") + #Creates individual graphs
+  coord_flip() + 
+  theme(legend.position = "None")
 
 
+df %>% 
+  unnest_tokens(word, "text", token = "ngrams", n =1) %>% 
+  count(character, word) %>% 
+  bind_tf_idf(word, character, n) %>% 
+  group_by(character) %>% 
+  top_n(tf_idf, n = 5) %>% 
+  ggplot(aes(x = reorder_within(word, tf_idf, character), y = tf_idf, fill = character)) + 
+  geom_col() + 
+  scale_x_reordered() + 
+  facet_wrap(~character, scales = "free") + 
+  coord_flip() + 
+  theme(legend.position = "none")
+
+bind_tf_idf()
+
+df %>% 
+  unnest_tokens(word, "text", token = "ngrams", n =2) %>% 
+  anti_join(stop_words) %>% 
+  count(character, word) %>% 
+  group_by(character) %>% 
+  top_n(n, n = 5) %>% 
+  arrange(character, -n) %>% 
+  ggplot(aes(x = reorder_within(word, n, character),#Reorders the words by freq
+             y  = n,
+             fill = character)) + 
+  geom_col() + 
+  scale_x_reordered() + #Reorders the words 
+  facet_wrap(~character, scales ="free") + #Creates individual graphs
+  coord_flip() + 
+  theme(legend.position = "None")
+  
 df %>% 
   unnest_tokens(trigram, "text", token = "ngrams", n = 3) %>% 
   count(character, trigram) %>% 
@@ -28,7 +75,6 @@ df %>%
   scale_x_reordered() +
   facet_wrap(~character, scales = "free") +
   coord_flip() + 
-  theme_fivethirtyeight() + 
   theme(legend.position = "None")
 
 
